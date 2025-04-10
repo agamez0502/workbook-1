f2078d2ea3199af262ac4d7a923283ab7751fe52
Alondra Gamez <agamez@appdev.yearup.org>
2025-04-10 11:43:47 -0500

    completed ShoppingList app

---
+++ shopping-list/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,18 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("\nHere is my Shopping List:\n");
+        System.out.println("Eggs");
+        System.out.println("Oat Milk");
+        System.out.println("Queso Fresco");
+        System.out.println("Oatmeal");
+        System.out.println("Baby Food");
+        System.out.println("Broccoli");
+        System.out.println("Bananas");
+        System.out.println("Chicken");
+        System.out.println("Turkey Bacon");
+        System.out.println("Steak");
+    }
+
+}
