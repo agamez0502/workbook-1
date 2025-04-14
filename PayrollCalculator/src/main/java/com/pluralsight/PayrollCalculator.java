89a065f3e1ddfde512d8d5733c13e361e8119e83
Alondra Gamez <agamez@appdev.yearup.org>
2025-04-14 15:55:48 -0500

    Completed Mod3 - Exercise 4 - Payroll Calculator

---
+++ PayrollCalculator/src/main/java/com/pluralsight/PayrollCalculator.java
@@ -0,0 +1,28 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class PayrollCalculator {
+    
+    public static void main(String[] args) {
+
+        Scanner myScanner = new Scanner(System.in);
+
+        System.out.println("This is a payroll calculator");
+        System.out.println("------------------------------------");
+
+        System.out.print("Enter your name: ");
+        String name = myScanner.nextLine();
+
+        System.out.print("Enter your hours worked: ");
+        float hoursWorked = myScanner.nextFloat();
+
+        System.out.print("Enter your pay rate: ");
+        float payRate = myScanner.nextFloat();
+
+        float grossPay = hoursWorked * payRate;
+        System.out.println("------------------------------------");
+        System.out.println(name + "'s gross pay is: " + grossPay);
+
+    }
+}
